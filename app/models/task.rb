25f14466f1ac678345be390d7046e711e4c846ea
emi ota <ametio.145@gmail.com>
2021-07-11 15:05:47 +0000

    Model Specを書いた

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   validates :title, {presence: true}
+  validates :content, {presence: true}
   validates :content, {length: {maximum:250} }
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -39,10 +39,10 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
   end
   describe '詳細表示機能' do
-     context '任意のタスク詳細画面に遷移した場合' do
-       it '該当タスクの内容が表示される' do
+    context '任意のタスク詳細画面に遷移した場合' do
+      it '該当タスクの内容が表示される' do
 
-       end
-     end
+      end
+    end
   end
 end
