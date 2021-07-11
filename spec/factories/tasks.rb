4d2b83678ca7f7a04055dcd59bfcd0273ba0c394
emi ota <ametio.145@gmail.com>
2021-07-11 11:01:24 +0000

    System Specを書いた

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :task do
+    # 下記の内容は実際に作成するカラム名に合わせて変更してください
+    title { 'Factoryで作ったデフォルトのタイトル１' }
+    content { 'Factoryで作ったデフォルトのコンテント１' }
+  end
+  # （存在しないクラス名の名前をつける場合、オプションで「このクラスのテストデータにしてください」と指定します）
+  factory :second_task, class: Task do
+    title { 'Factoryで作ったデフォルトのタイトル２' }
+    content { 'Factoryで作ったデフォルトのコンテント２' }
+  end
+end
