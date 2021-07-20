0778974c12c22ec761c6d017f7670fa39fb2584b
emi ota <ametio.145@gmail.com>
2021-07-20 07:13:55 +0000

    Model Specのテストを作成

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -22,4 +22,38 @@ describe 'タスクモデル機能', type: :model do
       end
     end
   end
+
+  describe '検索機能' do
+    # 必要に応じて、テストデータの内容を変更して構わない
+    let!(:task) { FactoryBot.create(:task) }
+    let!(:task2) { FactoryBot.create(:task2) }
+    let!(:task3) { FactoryBot.create(:task3) }
+    context 'scopeメソッドでタイトルのあいまい検索をした場合' do
+      it "検索キーワードを含むタスクが絞り込まれる" do
+        # title_seachはscopeで提示したタイトル検索用メソッドである。メソッド名は任意で構わない。
+        expect(Task.title_search('タイトル１')).to include(task)
+        expect(Task.title_search('タイトル１')).not_to include(task2)
+        expect(Task.title_search('タイトル１')).not_to include(task3)
+        expect(Task.title_search('タイトル１').count).to eq 1
+      end
+    end
+    context 'scopeメソッドでステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        # ここに内容を記載する
+        expect(Task.status_search('0')).to include(task)
+        expect(Task.status_search('0')).to include(task2)
+        expect(Task.status_search('0')).not_to include(task3)
+        expect(Task.status_search('0').count).to eq 2
+      end
+    end
+    context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        # ここに内容を記載する
+        expect(Task.title_search('タイトル１').status_search('0')).to include(task)
+        expect(Task.title_search('タイトル１').status_search('0')).not_to include(task2)
+        expect(Task.title_search('タイトル１').status_search('0')).not_to include(task3)
+        expect(Task.title_search('タイトル１').status_search('0').count).to eq 1
+      end
+    end
+  end
 end
