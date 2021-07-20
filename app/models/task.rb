5c086fd668693363255f654921bb5fc6e0b0b66e
emi ota <ametio.145@gmail.com>
2021-07-20 08:36:19 +0000

    優先順位カラムを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,21 +4,15 @@ class Task < ApplicationRecord
   validates :content, {length: {maximum:250} }
 
   enum status: {未着手:0, 着手中:1, 完了:2}
-#  def title_search(search)
-    #return Task.all unless search
-#    Task.where(['title LIKE ?', "%#{search}%"])
-#  end
+  enum priority: { 低:0, 中:1, 高:2}
 
   scope :title_search, -> (title) {
     where("title LIKE ?", "%#{ title }%")
   }
-
   scope :status_search, -> (status) {
     where(status: status)
   }
-
-  #def status_search(search)
-    #return Task.all unless search
-  #  Task.where(['status LIKE ?', "#{search}"])
-  #end
+  scope :priority_search, -> (priority) {
+    where(priority: priority)
+  }
 end
