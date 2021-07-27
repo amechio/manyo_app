22376045f109a4cf69f6d79967f72da7282a41fe
emi ota <ametio.145@gmail.com>
2021-07-27 05:22:24 +0000

    ユーザー管理機能を実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_24_132858) do
+ActiveRecord::Schema.define(version: 2021_07_27_025401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_07_24_132858) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "tasks", "users"
