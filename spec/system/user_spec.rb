13696bc422901e337c02d504c2ee0a6959d3d35b
emi ota <ametio.145@gmail.com>
2021-07-28 07:34:08 +0000

    RSpecをリファクタリング

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,16 +1,9 @@
 require 'rails_helper'
 RSpec.describe 'ユーザ機能', type: :system do
-  # before do
-  #   @user = FactoryBot.create(:user)
-  #   @user2 = FactoryBot.create(:user2)
-  #   @user3 = FactoryBot.create(:user3)
-  # #   # visit users_path
-  # end
 
   describe 'ユーザ登録機能' do
     context 'ユーザを新規作成した場合' do
       it '作成したユーザが表示される' do
-        # binding.irb
         visit new_user_path
         fill_in 'user[name]', with: '名前'
         fill_in 'user[email]', with: 'email@mail.com'
@@ -30,32 +23,21 @@ RSpec.describe 'ユーザ機能', type: :system do
   end
 
   describe 'セッション機能' do
-    # binding.irb
     before do
       @user = FactoryBot.create(:user)
       @user2 = FactoryBot.create(:user2)
       @user3 = FactoryBot.create(:user3)
-      # visit new_session_path
-      # fill_in 'session[email]', with: 'Factory1@mail.com'
-      # fill_in 'session[password]', with: 'Factory1'
-      # click_on 'Log in'
+      visit new_session_path
+      fill_in 'session[email]', with: 'factory1@mail.com'
+      fill_in 'session[password]', with: 'factory1'
+      click_on 'Log in'
     end
     context 'ログインしたとき' do
       it 'ようこそと表示される' do
-        # binding.irb
-        visit new_session_path
-        fill_in 'session[email]', with: 'factory1@mail.com'
-        fill_in 'session[password]', with: 'factory1'
-        click_on 'Log in'
         sleep 1
         expect(page).to have_content 'ようこそ'
       end
       it '自分の詳細画面(マイページ)に飛べる' do
-        visit new_session_path
-        fill_in 'session[email]', with: 'factory1@mail.com'
-        fill_in 'session[password]', with: 'factory1'
-        click_on 'Log in'
-        # sleep 1
         expect(page).to have_content 'のページ'
       end
       it '一般ユーザが他人の詳細画面に飛ぶとタスク一覧画面に遷移する' do
@@ -67,10 +49,6 @@ RSpec.describe 'ユーザ機能', type: :system do
         expect(page).to have_content '権限がありません'
       end
       it 'ログアウトができる' do
-        visit new_session_path
-        fill_in 'session[email]', with: 'factory1@mail.com'
-        fill_in 'session[password]', with: 'factory1'
-        click_on 'Log in'
         click_on 'Logout'
         expect(page).to have_content 'ログアウトしました'
       end
@@ -82,15 +60,14 @@ RSpec.describe 'ユーザ機能', type: :system do
       @user = FactoryBot.create(:user)
       @user2 = FactoryBot.create(:user2)
       @user3 = FactoryBot.create(:user3)
-    #   # visit users_path
+      visit new_session_path
+      fill_in 'session[email]', with: 'factory1@mail.com'
+      fill_in 'session[password]', with: 'factory1'
+      click_on 'Log in'
+      visit admin_users_path
     end
     context '管理ユーザが登録されているとき' do
       it '管理ユーザは管理画面にアクセスできる' do
-        visit new_session_path
-        fill_in 'session[email]', with: 'factory1@mail.com'
-        fill_in 'session[password]', with: 'factory1'
-        click_on 'Log in'
-        visit admin_users_path
         expect(page).to have_content '管理者画面index'
       end
       it '一般ユーザは管理画面にアクセスできない' do
@@ -102,38 +79,19 @@ RSpec.describe 'ユーザ機能', type: :system do
         expect(page).to have_content '権限がありません'
       end
       it '管理ユーザはユーザの新規登録ができる' do
-        visit new_session_path
-        fill_in 'session[email]', with: 'factory1@mail.com'
-        fill_in 'session[password]', with: 'factory1'
-        click_on 'Log in'
-        visit admin_users_path
         click_on 'ユーザの新規登録'
         expect(page).to have_content '管理者画面new'
       end
       it '管理ユーザはユーザの詳細画面にアクセスできる' do
-        visit new_session_path
-        fill_in 'session[email]', with: 'factory1@mail.com'
-        fill_in 'session[password]', with: 'factory1'
-        click_on 'Log in'
-        visit admin_users_path
         page.all('#show')[3].click  #user3の詳細ボタン
+        sleep 1
         expect(page).to have_content 'Factoryで作ったデフォルトの名前３のページ'
       end
       it '管理ユーザはユーザの編集画面からユーザを編集できる' do
-        visit new_session_path
-        fill_in 'session[email]', with: 'factory1@mail.com'
-        fill_in 'session[password]', with: 'factory1'
-        click_on 'Log in'
-        visit admin_users_path
         page.all('#edit')[3].click  #user3の編集ボタン
         expect(page).to have_content '管理者画面edit'
       end
       it '管理ユーザはユーザの削除をできる' do
-        visit new_session_path
-        fill_in 'session[email]', with: 'factory1@mail.com'
-        fill_in 'session[password]', with: 'factory1'
-        click_on 'Log in'
-        visit admin_users_path
         page.accept_confirm do
           page.all('#delete')[3].click  #user3の削除ボタン
         end
@@ -142,5 +100,3 @@ RSpec.describe 'ユーザ機能', type: :system do
     end
   end
 end
-
-# bundle exec rspec spec/system/user_spec.rb
