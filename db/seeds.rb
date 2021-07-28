eea9636c0e42b260ea5e1057c6f5b27ee8a9d617
emi ota <ametio.145@gmail.com>
2021-07-28 14:40:19 +0000

    seedsデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: "管理人1",
+  email: "user1@mail.com",
+  password: "password",
+  password_confirmation: "password",
+  admin: true
+)
+User.create!(
+  name: "初期ユーザ2",
+  email: "user2@mail.com",
+  password: "password",
+  password_confirmation: "password",
+  admin: false
+)
+User.create!(
+  name: "初期ユーザ",
+  email: "user3@mail.com",
+  password: "password",
+  password_confirmation: "password",
+  admin: false
+)
+
+User.all.each do |user|
+  user.tasks.create!(
+    title: "いっこめ",
+    content: "いっこめ",
+    limit: "2021-08-02",
+    status: "未着手",
+    priority: "高",
+  )
+  user.tasks.create!(
+    title: "にこめ",
+    content: "にこめ",
+    limit: "2021-08-03",
+    status: "着手中",
+    priority: "中",
+  )
+  user.tasks.create!(
+    title: "さんこめ",
+    content: "さんこめ",
+    limit: "2021-08-01",
+    status: "完了",
+    priority: "低",
+  )
+end
